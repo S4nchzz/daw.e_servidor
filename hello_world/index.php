2a0d084ccf13a4961c428406d10568b1e701e45a
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-09-19 13:55:41 +0200

    Array dimensional con string index

--- hello_world/index.php
+++ hello_world/index.php
@@ -10,16 +10,43 @@
         $HELLO = "Hello world <br/>";
 
         class Obj {
+            static function iAmStatic() {
+                echo "Soy un metodo estatico";  
+            }
+
             function hello() {
                 return "Hello from Obj <br/>";
             }
         }
+        
+        $obj = new Obj();
 
         for ($i = 0; $i < 5; $i++) {
             echo "$HELLO <br/>";
-            $obj = new Obj();
             echo $obj->hello();
         }
+
+        $arr = array("one", "two", "three");
+        for ($i = 0; $i < count($arr); $i++) {
+            echo "$arr[$i] ";
+        }
+
+        echo "<br/><br/><br/>";
+        
+        echo Obj::iAmStatic();
+        
+        echo "<br/><br/><br/>";
+
+        $dimarr = array(
+        "perros" => array(
+            "bulldog" => array("Kevin", "Tobi"),
+            "caniche" => array("Pepito", "Pepito2")),
+        "gatos" => array(
+            "siameses" => array("Otis", "Otis2"),
+            "persas" => array("Persas1", "Persas2")
+        ));
+
+        echo $dimarr["gatos"]["siameses"][1];
     ?>
 </body>
 </html>
