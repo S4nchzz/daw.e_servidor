64018cfce323c3df1e3c1f1414cc4ab4a9f16414
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-09-22 10:49:47 +0200

    FIX precision en round

--- php/math/index.php
+++ php/math/index.php
@@ -1,9 +1,9 @@
 <?php
     $valor_redondear = 50.525;
 
-    $to_odd = round($valor_redondear, 3, PHP_ROUND_HALF_ODD);
-    $to_even = round($valor_redondear, 3, PHP_ROUND_HALF_EVEN);
-    $to_up = round($valor_redondear, 3, PHP_ROUND_HALF_UP);
+    $to_odd = round($valor_redondear, 2, PHP_ROUND_HALF_ODD);
+    $to_even = round($valor_redondear, 2, PHP_ROUND_HALF_EVEN);
+    $to_up = round($valor_redondear, 2, PHP_ROUND_HALF_UP);
 
     echo "Redondeo a impar: $to_odd </br>";
     echo "Redondeo a par: $to_even </br>";
