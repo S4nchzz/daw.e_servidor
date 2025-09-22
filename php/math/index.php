26553815ea07c23e80dd43b60527cf36b1358d17
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-09-22 10:41:38 +0200

    Contenido movido a /php y codigo que usa round

---
+++ php/math/index.php
@@ -0,0 +1,11 @@
+<?php
+    $valor_redondear = 50.525;
+
+    $to_odd = round($valor_redondear, 3, PHP_ROUND_HALF_ODD);
+    $to_even = round($valor_redondear, 3, PHP_ROUND_HALF_EVEN);
+    $to_up = round($valor_redondear, 3, PHP_ROUND_HALF_UP);
+
+    echo "Redondeo a impar: $to_odd </br>";
+    echo "Redondeo a par: $to_even </br>";
+    echo "Redondeo al alto: $to_up </br>";
+?>
