37f15b0b6c2bd3404de789242820b183ad77147b
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-10-03 14:15:45 +0200

    Añadido operando

--- php/UT3/datos.php
+++ php/UT3/datos.php
@@ -1,3 +1,18 @@
 <?php
-    echo $_POST["n1"] + $_POST["n2"];
+    function getOperation() {
+        $n1 = $_POST["n1"];
+        $n2 = $_POST["n2"];
+        switch($_POST["operacion"]) {
+            case 'suma':
+                return $n1 + $n2;
+            case 'resta':
+                return $n1 - $n2;
+            case 'multiplicacion':
+                return $n1 * $n2;
+            case 'division':
+                return $n1 / $n2;
+        }
+    }
+
+    echo getOperation();
 ?>
