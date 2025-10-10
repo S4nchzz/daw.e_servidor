9b7ae5c2e924a689cd0417e56a2bbef2721b9614
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-10-10 14:17:14 +0200

    Hechos ejericios hasta el 19

--- php/UT3/practicas/practica2.php
+++ php/UT3/practicas/practica2.php
@@ -143,42 +143,24 @@ ejercicio4();
 echo "<br/><hr/>";
 
 function ejercicio5() {
+    $nRow = 3;
+    $nCol = 5;
     $matrix = array(
-        array(
-            array(),
-            array(),
-            array(),
-            array(),
-            array()
-        ),
-        array(
-            array(),
-            array(),
-            array(),
-            array(),
-            array()
-        ),
-        array(
-            array(),
-            array(),
-            array(),
-            array(),
-            array()
-        ),
+        array(),
+        array(),
+        array(),
     );
 
-    foreach ($matrix as $keyCol => $col) {
-        foreach ($col as $keyRow => $row) {
-            array_push($matrix[$keyCol][$keyRow], rand(1, 9));
+    foreach ($matrix as $keyRow => $row) {
+        for ($i = 0; $i < $nCol; $i++) {
+            array_push($matrix[$keyRow], rand(11, 99));
         }
     }
 
     echo 'Row: <br/><br/>';
-    foreach ($matrix as $keyCol => $col) {
-        foreach ($col as $keyRow => $row) {
-            foreach($row as $item) {
-                echo $item;
-            }
+    foreach ($matrix as $kr => $col) {
+        foreach ($col as $kc => $item) {
+            echo $item.' ';
         }
 
         echo '<br/>';
@@ -187,11 +169,11 @@ function ejercicio5() {
     echo '<br/>';
 
     echo 'Col: <br/><br/>';
-    foreach ($matrix as $keyCol => $col) {
-        foreach ($col as $keyRow => $row) {
-            echo $col[$keyRow][$keyCol];
-            echo '<br/>';
+    for ($r = 0; $r < $nCol; $r++) {
+        for ($c = 0; $c < $nRow; $c++) {
+            echo $matrix[$c][$r].' ';
         }
+
         echo '<br/>';
     }
 }
@@ -201,7 +183,47 @@ ejercicio5();
 echo "<br/><hr/>";
 
 function ejercicio6() {
+    $nRow = 4;
+    $nCol = 5;
+    $matrix = array(
+        array(),
+        array(),
+        array(),
+        array()
+    );
+
+    foreach ($matrix as $keyRow => $row) {
+        for ($i = 0; $i < $nCol; $i++) {
+            array_push($matrix[$keyRow], rand(11, 99));
+        }
+    }
+
+    echo 'Row: <br/><br/>';
+    foreach ($matrix as $keyRow => $row) {
+        foreach ($row as $keyCol => $col) {
+            echo $col.' ';
+        }
+
+        echo '<br/>';
+    }
     
+    echo '<br/>';
+    
+    $bigger = 0;
+    $bigRow = 0;
+    $bigCol = 0;
+    foreach ($matrix as $kr => $row) {
+        foreach ($row as $kc => $value) {
+            if ($value > $bigger) {
+                $bigger = $value;
+                $bigRow = $kr;
+                $bigCol = $kc;
+            }
+        }
+    }
+
+    echo 'El numero mas grande es: '.$bigger.' | Row: '.$bigRow.' | Col:'.$bigCol;
+
 }
 echo "<h2>Ejercicio 6</h2>";
 echo "<p>Genera una matriz 4x5 con valores aleatorios y determina la fila y columna del elemento mayor.</p>";
@@ -209,7 +231,42 @@ ejercicio6();
 echo "<br/><hr/>";
 
 function ejercicio7() {
-    
+    $nRow = 3;
+    $nCol = 4;
+    $matrix = array(
+        array(),
+        array(),
+        array()
+    );
+
+    foreach ($matrix as $keyRow => $row) {
+        for ($i = 0; $i < $nCol; $i++) {
+            array_push($matrix[$keyRow], rand(11, 99));
+        }
+    }
+
+    $big = 0;
+    $lastBig = $big;
+
+    foreach ($matrix as $keyRow => $row) {
+        foreach ($row as $keyCol => $col) {
+            if ($col > $big) {
+                $lastBig = $big;
+                $big = $col;
+            }
+
+            echo $col.' ';
+        }
+
+        echo '<br/>';
+        echo 'Mayor: '.$big.' | Mayor anterior: '.$lastBig;
+        echo '<br/>';
+        echo 'Promedio: '.(($big + $lastBig) / 2);
+        echo '<br/><br/>';
+
+        $big = 0;
+        $lastBig = 0;
+    }
 }
 echo "<h2>Ejercicio 7</h2>";
 echo "<p>Genera una matriz 3x4 y crea dos vectores: uno con los valores máximos de cada fila y otro con los promedios. Muéstralos por pantalla.</p>";
@@ -217,7 +274,35 @@ ejercicio7();
 echo "<br/><hr/>";
 
 function ejercicio8() {
-    
+    $nRow = 10;
+    $nCol = 10;
+    $matrix = array();
+
+    for($i = 0; $i < $nRow; $i++) {
+        $arr = array();
+        for($k = 0; $k < $nCol; $k++) {
+            array_push($arr, rand(11, 99));
+        }
+
+        array_push($matrix, $arr);
+    }
+
+    $bigger = 0;
+    $bigRow = 0;
+    $bigCol = 0;
+    foreach ($matrix as $kr => $row) {
+        foreach ($row as $kc => $col) {
+            if ($col > $bigger) {
+                $bigRow = $kr;
+                $bigCol = $kc;
+                $bigger = $col;
+            }
+            echo $matrix[$kr][$kc].' ';
+        }
+        echo '<br/>';
+    }
+
+    echo '<br/>N Mayor: '.$bigger.' | Row: '.$bigRow.' | Col: '.$bigCol;
 }
 echo "<h2>Ejercicio 8</h2>";
 echo "<p>Llena una matriz 10x10 con valores aleatorios y muestra la posición (fila, columna) del número mayor.</p>";
@@ -225,7 +310,38 @@ ejercicio8();
 echo "<br/><hr/>";
 
 function ejercicio9() {
+    $nRow = 5;
+    $nCol = 5;
+    $matrix = array();
+
+    for($i = 0; $i < $nRow; $i++) {
+        $arr = array();
+        for($k = 0; $k < $nCol; $k++) {
+            array_push($arr, rand(11, 99));
+        }
+
+        array_push($matrix, $arr);
+    }
+
+    $sumArr = array();
+    foreach ($matrix as $kr => $row) {
+        $sum = 0;
+        foreach ($row as $kc => $col) {
+            echo $matrix[$kc][$kr].' ';
+            $sum += $col;
+        }
+
+        array_push($sumArr, $sum);
+        echo '<br/>';
+    }
     
+    echo '<br/>Columnas sumadas: ';
+    foreach ($sumArr as $key => $sumItem) {
+        echo $key.' = '.$sumItem.' | ';
+    }
+    echo '<br/>';
+
+    echo 'Mayor: '.max($sumArr);
 }
 echo "<h2>Ejercicio 9</h2>";
 echo "<p>Llena una matriz 20x20 con valores aleatorios. Suma las columnas e imprime la que tenga la máxima suma y su valor.</p>";
@@ -233,7 +349,16 @@ ejercicio9();
 echo "<br/><hr/>";
 
 function ejercicio10() {
-    
+    $v = array(
+        1 => 90,
+        30 => 7,
+        'e' => 99,
+        'hola' => 43
+    );
+
+    foreach ($v as $key => $value) {
+        echo $key.': '.$value.' <br/>';
+    }
 }
 echo "<h2>Ejercicio 10</h2>";
 echo "<p>Carga un array asociativo con los valores dados y muestra su contenido usando foreach.</p>";
@@ -241,7 +366,23 @@ ejercicio10();
 echo "<br/><hr/>";
 
 function ejercicio11() {
-    
+    $monthMovies = array(
+        "Enero" => array("Oppenheimer", "Wonka", "La sociedad de la nieve"), 
+        "Febrero" => array("Dune: Parte Dos", "Madame Web", "Bob Marley: One Love"), 
+        "Marzo" => array(), 
+        "Abril" => array("Civil War", "Challengers", "Monkey Man")
+    );
+
+    foreach($monthMovies as $key => $month) {
+        if (!empty($month)) {
+            echo $key.': <br/>';
+            foreach ($month as $movieKey => $movie) {
+                echo $movie.'<br/>';
+            }
+
+            echo '<br/>';
+        }
+    }
 }
 echo "<h2>Ejercicio 11</h2>";
 echo "<p>Crea un array con las películas vistas por mes (enero a abril). Muestra solo los meses con películas vistas.</p>";
@@ -249,7 +390,16 @@ ejercicio11();
 echo "<br/><hr/>";
 
 function ejercicio12() {
-    
+    $person = array(
+        "Name" => "Iyan",
+        "Surname" => "Sanchez",
+        "Address" => "Aviles",
+        "Phone" => "666"
+    );
+
+    foreach ($person as $key => $p) {
+        echo $key.': '.$p.' <br/>';
+    }
 }
 echo "<h2>Ejercicio 12</h2>";
 echo "<p>Crea un array asociativo con los datos de una persona (nombre, dirección y teléfono) y muéstralos.</p>";
@@ -257,7 +407,11 @@ ejercicio12();
 echo "<br/><hr/>";
 
 function ejercicio13() {
-    
+    $cities = array("Aviles", "Gijon", "Oviedo", "Nueva york");
+
+    foreach ($cities as $key => $city) {
+        echo 'La ciudad '.$city.' esta en la posicion '.$key.'<br/>';
+    }
 }
 echo "<h2>Ejercicio 13</h2>";
 echo "<p>Crea un array con ciudades y muestra cada una indicando su índice.</p>";
@@ -265,7 +419,11 @@ ejercicio13();
 echo "<br/><hr/>";
 
 function ejercicio14() {
-    
+    $cities = array("AVS" => "Aviles", "GJN" => "Gijon", "OVD" => "Oviedo", "NY" => "Nueva york", "MD" => "Madrid");
+
+    foreach ($cities as $key => $city) {
+        echo 'La ciudad '.$city.' con abreviatura '.$key.'<br/>';
+    }
 }
 echo "<h2>Ejercicio 14</h2>";
 echo "<p>Repite el ejercicio anterior pero usando índices asociativos (por ejemplo, 'MD' para Madrid).</p>";
@@ -273,7 +431,16 @@ ejercicio14();
 echo "<br/><hr/>";
 
 function ejercicio15() {
-    
+    $names = array("Pedro", "Ismael", "Sonia", "Clara", "Susana", "Alfonso", "Teresa");
+
+    echo 'N elementos: '.count($names);
+    echo '<ul>';
+    foreach ($names as $key => $name) {
+        echo '<li>';
+        echo $name;
+        echo '</li>';
+    }
+    echo '</ul>';
 }
 echo "<h2>Ejercicio 15</h2>";
 echo "<p>Crea un array con nombres y muestra el número de elementos y cada uno en una lista no numerada HTML.</p>";
@@ -281,7 +448,22 @@ ejercicio15();
 echo "<br/><hr/>";
 
 function ejercicio16() {
+    $lenguajes_cliente = array("JAVASCRIPT", "TYPESCRIPT");
+    $lenguajes_servidor = array("Java", "PHP", "ASP");
+
+    $mix = array();
     
+    foreach ($lenguajes_cliente as $key => $lang) {
+        array_push($mix, $lang);
+    }
+    
+    foreach ($lenguajes_servidor as $key => $lang) {
+        array_push($mix, $lang);
+    }
+    
+    foreach ($mix as $key => $languajes) {
+        echo $languajes.' | ';
+    }
 }
 echo "<h2>Ejercicio 16</h2>";
 echo "<p>Crea dos arrays de lenguajes (cliente y servidor), únelos en uno solo y muéstralos en una tabla.</p>";
@@ -289,7 +471,15 @@ ejercicio16();
 echo "<br/><hr/>";
 
 function ejercicio17() {
-    
+    $array1 = array(1, 2, 3);
+    $array2 = array(4, 5, 6);
+    $array3 = array(7, 8, 9);
+
+    $merged = array_merge($array1, $array2, $array3);
+
+    foreach ($merged as $key => $item) {
+        echo $item.' ';
+    }
 }
 echo "<h2>Ejercicio 17</h2>";
 echo "<p>Rellena tres arrays y únelos en uno nuevo usando array_merge(). Muéstralo por pantalla.</p>";
@@ -297,7 +487,18 @@ ejercicio17();
 echo "<br/><hr/>";
 
 function ejercicio18() {
-    
+    $array1 = array(1, 2, 3);
+    $array2 = array(4, 5, 6);
+    $array3 = array(7, 8, 9);
+
+    $merged = array();
+    array_push($merged, $array1, $array2, $array3);
+
+    foreach ($merged as $key => $arr) {
+        foreach ($arr as $key => $item) {
+            echo $item.' ';
+        }
+    }
 }
 echo "<h2>Ejercicio 18</h2>";
 echo "<p>Realiza el ejercicio anterior pero usando array_push() en lugar de array_merge().</p>";
@@ -305,7 +506,19 @@ ejercicio18();
 echo "<br/><hr/>";
 
 function ejercicio19() {
-    
+    $array1 = array(1, 2, 3);
+    $array2 = array(4, 5, 6);
+    $array3 = array(7, 8, 9);
+
+    $merged = array();
+    array_push($merged, $array1, $array2, $array3);
+    rsort($merged);
+
+    foreach ($merged as $key => $arr) {
+        foreach ($arr as $key => $item) {
+            echo $item.' ';
+        }
+    }
 }
 echo "<h2>Ejercicio 19</h2>";
 echo "<p>Muestra el array del ejercicio anterior pero en orden inverso.</p>";
