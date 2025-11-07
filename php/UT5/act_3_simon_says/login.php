36fbbc4141d64f3fd9e882e1f727a5cc091352f3
iyannsanchezzdc4@gmail.com <iyannsanchezzdc4@gmail.com>
2025-11-07 12:02:52 +0100

    Creado login que redirige al start.php si las credenciales en texto plano son correctas

---
+++ php/UT5/act_3_simon_says/login.php
@@ -0,0 +1,50 @@
+<?php
+    function printForm() {
+        echo <<<_END
+            <html>
+                <head>
+                    <title>Login</title>
+                </head>
+                <body>
+                    <form action="login.php" method="post">
+                        <input type="text" placeholder="Usuario" name="user"/>
+                        <input type="text" placeholder="Contraseña" name="pass"/>
+                        <input type="submit" value="Entrar"/>
+                    </form>
+                <body/>
+            </html>
+            _END;
+    }
+
+    function recharge() {   
+        if (isset($_POST['user'])) {
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+
+            $un = 'root';
+            $db = 'bdsimon';
+            $hn = 'localhost';
+            $pw = '';
+
+            $conn = new mysqli($hn, $un, $pw, $db);
+            if ($conn->connect_error) die('Fatal error');
+
+            $query = "SELECT * FROM usuarios WHERE nombre = '$user' AND clave = '$pass'";
+            $result = $conn->query($query);
+            if (!$result) echo 'Usuario no encontrado';
+
+            if ($result->num_rows > 0) {
+                header("Location: start.php");
+                exit();
+            }
+
+            printForm();
+            echo "No se ha encontrado el usuario";
+            return;
+        }
+        
+        printForm();
+    }
+
+    recharge();
+?>
